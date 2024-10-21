e720041097efcbf991b3cb3cd88041e4e2355285
grayxu <mail@grayxu.cn>
2024-10-22 00:01:48 +0800

    fix: the witcher 3 (switch->pc) close #1

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 
 A tool to sync save files between Nintendo Switch and PC.
 
-You can use [JKSV](https://github.com/J-D-K/JKSV) to export and upload zipped switch saves, then convert and sync PC saves with NS-Cross-Save-Sync. Or conversely, convert PC saves to zipped switch saves (*note that due to the current limitations of JKSV, only same-name overwriting is possible*).
+You can use [*JKSV*](https://github.com/J-D-K/JKSV) to export and upload zipped switch saves, then convert and sync PC saves with *NS-Cross-Save-Sync*. Or conversely, convert PC saves to zipped switch saves (*note that due to the current limitations of JKSV, only same-name overwriting is possible*).
 
-A best practice is to mount webdav locally and use NS-Cross-Save-Sync for bidirectional save synchronization.
+A best practice is to mount WebDAV locally and use *NS-Cross-Save-Sync* for bidirectional save synchronization.
 
 ## run
 
 ```bash
 git clone https://github.com/NS-cross-save-games/NS-Cross-Save-Sync.git
-cd dist
 ./main.exe
 ```
 
--- converter/the_witcher_3.py
+++ converter/the_witcher_3.py
@@ -20,13 +20,22 @@ class the_witcher_3(Converter):
         if not os.path.exists(tmp_path):
             os.makedirs(tmp_path)
 
+        # collect
+        save_files = []
         for fname in os.listdir(os.path.join(pc_folder_path, 'gamesaves')):
             if fname.endswith('.png'):
-                basename = os.path.splitext(fname)[0]
-                shutil.copy(os.path.join(pc_folder_path, 'gamesaves', basename+'.png'), tmp_path)
-                shutil.copy(os.path.join(pc_folder_path, 'gamesaves', basename+'.sav'), tmp_path)
-                with open(os.path.join(tmp_path, basename + '.req'), 'w', encoding='utf-8') as req_file:
-                    req_file.write("content0;content1;content2;content3;content4;")
+                full_path = os.path.join(pc_folder_path, 'gamesaves', fname)
+                save_files.append((fname, os.path.getmtime(full_path)))
+        # sort by mtime
+        save_files.sort(key=lambda x: x[1], reverse=True)
+
+        # only copy the first 10
+        for fname, _ in save_files[:10]:
+            basename = os.path.splitext(fname)[0]
+            shutil.copy2(os.path.join(pc_folder_path, 'gamesaves', basename+'.png'), tmp_path)
+            shutil.copy2(os.path.join(pc_folder_path, 'gamesaves', basename+'.sav'), tmp_path)
+            # with open(os.path.join(tmp_path, basename + '.req'), 'w', encoding='utf-8') as req_file:
+            #     req_file.write("content0;content1;content2;content3;content4;")
         
         # copy settings
         tmp_path = 'saves/pc/The Witcher 3/user.settings/'
@@ -64,12 +73,17 @@ class the_witcher_3(Converter):
         tmp_path = os.path.join(pc_folder_path, 'gamesaves')
         if not os.path.exists(tmp_path):
             os.makedirs(tmp_path)
+        if not os.path.exists("offzip"):
+            os.makedirs("offzip")
 
         for fname in os.listdir(switch_extraction_path):
             if fname.endswith('.png'):
                 basename = os.path.splitext(fname)[0]
-                shutil.copy(os.path.join(switch_extraction_path, basename+'.png'), tmp_path)
-                shutil.copy(os.path.join(switch_extraction_path, basename+'.sav'), tmp_path)
+                shutil.copy2(os.path.join(switch_extraction_path, basename+'.png'), tmp_path)
+                # use offzip to extract the .sav file
+                # Note: If the versions don't match, you can change the value at offset C16 to 0 in hex edit.
+                os.system(f".\\offzip.exe -a {os.path.join(switch_extraction_path, basename + '.sav')} offzip 0")
+                shutil.move(os.path.join('offzip', "0000000c.snf"), os.path.join(tmp_path, basename + '.sav'))
                 
         return True
 
